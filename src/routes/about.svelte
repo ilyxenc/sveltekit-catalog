83402df2fcf72ef3d27b5938e8f085117acef13a
ilyxenc <ilya.goldobin1@gmail.com>
2022-02-07 19:28:10 +0300

    main

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -9,6 +9,6 @@
 <Menu {active} />
 <div class="px-4 py-5 my-5 text-center">
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Little catalog as example</p>
+      <p class="lead mb-4">Little catalog with client side filtering and routing as example</p>
     </div>
   </div>
